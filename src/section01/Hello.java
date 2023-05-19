17e54504dbed1e40daa45c18d2172ce01b7fcbd2
user <user@DESKTOP-DOKEURI>
2023-05-19 19:55:03 +0900

    주석 추가

--- src/section01/Hello.java
+++ src/section01/Hello.java
@@ -1,8 +1,21 @@
 package section01;
+/* 
+ * 주석
+ *  코드에 영향을 주지 않는 개발자가 사용하는 메모공간
+ *  
+ *  여러줄 주석
+ */
 
 public class Hello {
 	public static void main(String[] args) {
+		// 한줄 주석
 		System.out.println("Hello, Java!");
+		
+		printNum(516);
+	}
+	
+	public static void printNum(int num) {
+		System.out.println("num :" + num);
 	}
 
 }
