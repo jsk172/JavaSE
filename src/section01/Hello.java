925f267a13fab157bdaae4868d7c46b72e3fc23c
user <user@DESKTOP-DOKEURI>
2023-05-19 19:57:56 +0900

    주석 추가

--- src/section01/Hello.java
+++ src/section01/Hello.java
@@ -13,6 +13,12 @@ public class Hello {
 		
 		printNum(516);
 	}
+	/** JavaDoc 주석
+	 * 
+	 * 입력받은 정수 출력하는 메소드
+	 * 
+	 * @param num - 입력받은 정수
+	 */
 	
 	public static void printNum(int num) {
 		System.out.println("num :" + num);
